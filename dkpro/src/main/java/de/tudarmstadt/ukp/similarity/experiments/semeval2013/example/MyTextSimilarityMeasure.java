84337b4faeac4e0b6e9d414df8ff3f3d7bb0f60a
Gleb Sizov <glebsv@gmail.com>
2013-02-19 23:26:16 +0100

    dkpro maven project
    Originally from here:
    http://code.google.com/p/dkpro-similarity-asl/wiki/SemEval2013 with
    small changes.

---
+++ dkpro/src/main/java/de/tudarmstadt/ukp/similarity/experiments/semeval2013/example/MyTextSimilarityMeasure.java
@@ -0,0 +1,30 @@
+package de.tudarmstadt.ukp.similarity.experiments.semeval2013.example;
+
+import de.tudarmstadt.ukp.similarity.algorithms.api.SimilarityException;
+import de.tudarmstadt.ukp.similarity.algorithms.api.TextSimilarityMeasureBase;
+
+import java.util.Collection;
+
+public class MyTextSimilarityMeasure
+	extends TextSimilarityMeasureBase
+{
+	@SuppressWarnings("unused")
+	private int n;
+	
+	public MyTextSimilarityMeasure(int n)
+	{
+		// The configuration parameter is not used right now and intended for illustration purposes only.
+		this.n = n;
+	}
+	
+	@Override
+	public double getSimilarity(Collection<String> stringList1,
+			Collection<String> stringList2)
+		throws SimilarityException
+	{
+
+		// Your similarity computation goes here.
+		return 1.0;
+	}
+
+}
